fe23f97ca1496f87fd9ea4318e53df450717f444
Stephane Baroux <stephane.baroux@gmail.com>
2021-09-12 18:26:26 +0200

    added first basic functions to render the list of lists, also function to take value from input field and create it into a list, stores to local storage and re-renders

--- script.js
+++ script.js
@@ -1,1 +1,52 @@
-console.log('script is working')
+const listsContainer = document.querySelector('[data-list]')
+const newListForm = document.querySelector('[data-new-list-form]')
+const newListInput = document.querySelector('[data-new-list-input]')
+
+const LOCAL_STORAGE_LIST_KEY = 'task.lists'
+let lists = JSON.parse(localStorage.getItem(LOCAL_STORAGE_LIST_KEY)) || []
+
+
+
+newListForm.addEventListener('submit', e => {
+    e.preventDefault()
+    let listName = newListInput.value
+    if(listName === null || listName === '') return
+    console.log(listName)
+    const list = createList(listName)
+    lists.push(list)
+    newListInput.value = null
+    saveAndRender()
+})
+
+const createList = (name) => {
+    console.log(name)
+    return { id: Date.now().toString(), name: name, tasks: [] }
+}
+
+const save = () => {
+    localStorage.setItem(LOCAL_STORAGE_LIST_KEY,JSON.stringify(lists))
+}
+
+const render = () => {
+    clearElement(listsContainer)
+    lists.forEach(list => {
+        const listElement = document.createElement('li')
+        listElement.dataset.listId = list.id
+        listElement.classList.add('list-name')
+        listElement.innerText = list.name
+        listsContainer.appendChild(listElement)
+    })
+}
+
+const saveAndRender = () => {
+    save()
+    render()
+}
+
+const clearElement = (element) => {
+    while (element.firstChild) {
+        element.removeChild(element.firstChild)
+    }
+}
+
+render()
