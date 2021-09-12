581f725227f6b59c4db7ecc700c9b1da4fdd3b1c
Stephane Baroux <stephane.baroux@gmail.com>
2021-09-13 00:33:30 +0200

    js done for the list creation and deletion. Next need to work on the task creation and interactivity

--- script.js
+++ script.js
@@ -1,11 +1,26 @@
 const listsContainer = document.querySelector('[data-list]')
 const newListForm = document.querySelector('[data-new-list-form]')
 const newListInput = document.querySelector('[data-new-list-input]')
+const deleteListBtn = document.querySelector('[data-delete-list-btn]')
 
 const LOCAL_STORAGE_LIST_KEY = 'task.lists'
+const LOCAL_STORAGE_SELECTED_LIST_ID_KEY = 'task.selectedListId'
+
 let lists = JSON.parse(localStorage.getItem(LOCAL_STORAGE_LIST_KEY)) || []
+let selectedListId = JSON.parse(localStorage.getItem(LOCAL_STORAGE_SELECTED_LIST_ID_KEY))
 
+listsContainer.addEventListener('click', e => {
+    if(e.target.tagName.toLowerCase() === 'li'){
+        selectedListId = e.target.dataset.listId
+        saveAndRender()
+    }
+})
 
+deleteListBtn.addEventListener('click', e => {
+    lists = lists.filter(list => list.id !== selectedListId)
+    selectedListId = null
+    saveAndRender()
+})
 
 newListForm.addEventListener('submit', e => {
     e.preventDefault()
@@ -25,6 +40,8 @@ const createList = (name) => {
 
 const save = () => {
     localStorage.setItem(LOCAL_STORAGE_LIST_KEY,JSON.stringify(lists))
+    localStorage.setItem(LOCAL_STORAGE_SELECTED_LIST_ID_KEY,JSON.stringify(selectedListId))
+
 }
 
 const render = () => {
@@ -34,6 +51,9 @@ const render = () => {
         listElement.dataset.listId = list.id
         listElement.classList.add('list-name')
         listElement.innerText = list.name
+        if(list.id === selectedListId){
+            listElement.classList.add('active-list')
+        }
         listsContainer.appendChild(listElement)
     })
 }
